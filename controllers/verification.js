0ae9d9af39cea1722cda46a4251a5a43774b0415
Soumyajit Pathak <s.pathak5995@gmail.com>
2017-08-20 19:08:59 +0530

    verify token

--- controllers/verification.js
+++ controllers/verification.js
@@ -1,7 +1,8 @@
+request('dotenv').config({ path: 'variables.env' });
 module.exports = (req, res) => {
 	const hubChallenge = req.query['hub.challenge'];
 	const hubMode = req.query['hub.mode'];
-	const verifyTokenMatches = (req.query['hub.verify_token'] === "It's Yummy!");
+	const verifyTokenMatches = (req.query['hub.verify_token'] === process.env.VERIFY_TOKEN);
 
 	if (hubMode && verifyTokenMatches) {
 		res.status(200).send(hubChallenge);
